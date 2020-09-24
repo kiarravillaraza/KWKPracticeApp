ddab383355dc066404bba19a853151cdafe561c0
Kiarra Villaraza <1010kiarra@gmail.com>
2020-09-24 16:35:51 -0500

    Added documentation file.

---
+++ KWKPracticeApp/Documentation.swift
@@ -0,0 +1,17 @@
+//
+//  Documentation.swift
+//  KWKPracticeApp
+//
+//  Created by Kiarra  on 9/24/20.
+//  Copyright © 2020 Kiarra . All rights reserved.
+//
+
+import Foundation
+
+//This app was inspired by Buzzfeed's quiz feature.
+//The user will begin on the opening page.
+//The user will then be presented with a series of questions.
+//The user can push each button to see the picture and text change according
+//to what they choose.
+//Once the user choses an option they will continue on until all of the questions are answered.
+//After the user is finished answering a series of questions, they will generate their answer by clicking the generate button. 
